730150e5dac7aded0c2c5db66d4816d8121bbb66
yjalil <jalil.yahy@gmail.com>
2023-02-11 10:35:51 +0100

    layout

--- pages/Explore_articles.py
+++ pages/Explore_articles.py
@@ -18,9 +18,11 @@ st.sidebar.header("Company Articles")
 st.write(
     """You can delete articles that might seem problematic"""
 )
-
-start_date = st.date_input('Start date')
-end_date = st.date_input('End date')
+col1, col2 = st.columns(2)
+with col1 :
+    start_date = st.date_input('Start date')
+with col2 :
+    end_date = st.date_input('End date')
 company = st.selectbox(
     'Company',
     tuple(tuple_companies))
@@ -32,8 +34,11 @@ if st.button('Show Articles'):
     for article in response.json():
         with st.expander(label = f"{article['id']} : {article['date']} | {article['title']})"):
             st.markdown(f"[Read more]({article['sourceURL']})")
-            st.metric(label="Topic", value=article['topic_category'])
-            st.metric(label="Sentiment", value=article['esg_score'])
+            col1, col2 = st.columns(2)
+            with col1 :
+                st.metric(label="Topic", value=article['topic_category'])
+            with col2 :
+                st.metric(label="Sentiment", value=article['esg_score'])
             # if st.button(label="Delete Article", key = f"Delete Article {article['id']}"):
             #     requests.delete(f"{BASE_URI}/article/delete/{article['id']}")
             if st.button(label="Flag for Retraining", key = f"Flag for Retraining {article['id']}"):
