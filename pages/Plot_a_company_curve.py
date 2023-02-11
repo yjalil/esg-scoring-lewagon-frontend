dbbad53fa182579049c63a02ceaa2d6e9a3883f1
yjalil <jalil.yahy@gmail.com>
2023-02-11 12:51:56 +0100

    plot

--- pages/Plot_a_company_curve.py
+++ pages/Plot_a_company_curve.py
@@ -4,6 +4,17 @@ import json
 import pandas as pd
 from config import BASE_URI
 
+
+def map_topic_weight(text):
+    if text == 'Social':
+        return 0.31
+    if text == 'Environmental':
+        return 0.44
+    if text == 'Governance':
+        return 0.25
+    else:
+        return 0.0
+    
 st.set_page_config(page_title="Plotting Company ESG trend", page_icon="📈")
 
 
@@ -29,5 +40,11 @@ company = st.selectbox(
 if st.button('Plot'):
     response_plot = requests.get(f"{BASE_URI}/articles/byCompany/{company}/Period/{start_date}/{end_date}")
     df = pd.DataFrame(response_plot.json())
-    st.dataframe(df)
-    
+    # st.write(df['date'].dtype)
+    df['weights'] = df['topic_category'].apply(map_topic_weight)
+    df['esg_agg_score'] = df['weights'] * df['esg_score']
+    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+    df.sort_values(by='date', ascending=True, inplace=True)
+    df['cumul'] = df['esg_score'].cumsum()
+    # st.dataframe(df)
+    st.line_chart(data=df, x = 'date', y= 'cumul' )
